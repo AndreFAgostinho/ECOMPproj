011026a8817acb3a0463eaed8bbbcee178f8753b
André Agostinho <andre.f.agostinho@tecnico.ulisboa.pt>
2018-11-07 10:20:43 +0000

    Added missing files and starded module declarations

---
+++ Code/rtl/xps2.v
@@ -0,0 +1,7 @@
+`timescale 1ns / 1ps
+
+module xps2 (
+			input wire clk, // Clock pin form keyboard
+			input wire data, //Data pin form keyboard
+			output reg [8:0] data_out // Keypress data output and valid bit
+	      );
---
+++ Code/rtl/xpushs.v
@@ -0,0 +1,7 @@
+`timescale 1ns / 1ps
+
+module xpush (
+			input wire push_C, // Push button for C
+			input wire push_CE, // Push button for CE
+			output reg [8:0] data_out // data output
+	      );
