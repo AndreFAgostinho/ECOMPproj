d3c68762ccca685aaa53767839d9283378b0d5a5
micasserodio <micaelaserodio08@gmail.com>
2018-12-12 02:33:06 +0000

    new xoper.v tested with a simple operation in sim, ready to test with the fpga

--- Code/rtl/xoper.v
+++ Code/rtl/xoper.v
@@ -28,96 +28,71 @@ module xoper(
 			
 
     );
-integer scan_counter =0;
-integer n_enter = 0;
+	 
+	 
+
+
 reg[10:0] operand1 = 11'b0;
 reg[10:0] operand2 = 11'b0;
 reg negative1;
 reg negative2; 
+reg [3:0] counter = 4'b0;
 reg[1:0] operator = 2'b0;
-reg [31:0] temp =32'b0;
+reg [31:0] temp ;
+
+reg [31:0] temp1 =32'b0;
+
+
 
 always @(negedge(sel))
 	begin
-
-	if (n_enter==0 && data_in == 11'b1110)
-		begin
-		scan_counter = 4; // jumps to the operator
-		n_enter = n_enter+1;
+	if (data_in == 11'b1110 && counter <4'b0100) counter = 4'b0100;
+	else if (data_in == 11'b1110 && counter >11'b0110 && counter < 11'b1001) counter = 4'b1001;
+	case(counter)
+	4'b0000: begin 
+	if (data_in == 11'b1010) negative1 <= 1'b0;
+	else if (data_in == 11'b1011) negative1 <= 1'b1;
+	end
+	4'b0001: operand1 <= data_in;
+	4'b0010: begin
+	temp = (operand1*11'b00000001010);
+	operand1 <= temp[10:0]+data_in; //tens	
+	end
+	4'b0011: begin
+		temp1 =(operand1*11'b00000001010);
+		operand1<= temp1[10:0]+data_in;
 		end
-	else if (n_enter ==1 && data_in == 11'b1110)
+	4'b0100:
 		begin 
-		scan_counter=5; // jumps to the second operand
-		n_enter = n_enter +1;
-		end
-	else if (n_enter ==2 && data_in == 11'b1110)
-		begin 
-		scan_counter=9; //jumps to the operation
-		n_enter = n_enter +1;
-		end
-
-	if (scan_counter == 0) 
-		begin 
-		if (data_in == 11'b1010) negative1 = 1'b0;
-		else if (data_in == 11'b1011) negative1 = 1'b1;
-		end
-
-	else if (scan_counter == 1) operand1 = data_in; //units	
-
-	else if (scan_counter == 2)
-		begin
-		temp = (operand1*11'b00000001010);
-		operand1 = temp[10:0]+data_in; //tens	
-		end	
-
-	else if (scan_counter == 3) 
-		begin 
-		if (negative1 ==1'b1) //ones
-			begin
-			temp=(operand1*11'b00000001010);
-			operand1=-(temp[10:0]+data_in); 
-			end
-		else operand1= temp[10:0]+data_in;
-		end
-
-	else if (scan_counter  == 4) //operator
-		begin 
+		
 		case (data_in)
-			11'b1010 : operator = 2'b00; //add
-			11'b1011: operator = 2'b01; //sub
-			11'b1100: operator = 2'b10; //mult
-			11'b1101: operator = 2'b11; //div
+			11'b1010 : operator <= 2'b00; //add
+			11'b1011: operator <= 2'b01; //sub
+			11'b1100: operator <= 2'b10; //mult
+			11'b1101: operator <= 2'b11; //div
 		endcase
 		end
-
-	else if (scan_counter == 5)
+	4'b0101:
 		begin
-		if (data_in == 11'b1010) negative2 = 1'b0;
-		else if (data_in == 11'b1011) negative2 = 1'b1;
+		if (data_in == 11'b1010) negative2 <= 1'b0;
+		else if (data_in == 11'b1011) negative2 <= 1'b1;
 		end
-	
-	else if (scan_counter == 6) operand2 = data_in; //units
-	
-	else if (scan_counter == 7) 
+	4'b0110: operand2 <= data_in; 
+	4'b0111:
 		begin
 		temp=(operand2*11'b00000001010);
-		operand2 = temp[10:0]+data_in; //tens	
+		operand2 <= temp[10:0]+data_in; 
 		end	
-	
-	else if (scan_counter == 8) //ones
+	4'b1000:
 		begin 
-		if (negative2 ==1'b1)
-			begin
-			temp= (operand2*11'b00000001010);
-			operand2=-(temp[10:0]+data_in); 
-			end
-		else operand2 = temp[10:0]+data_in;
+		temp1=(operand2*11'b00000001010);
+		operand2 <= temp1[10:0]+data_in;
 		end
-	
-	else if (scan_counter == 9)
+	 4'b1001:
 		begin 
-		if (data_in == 11'b1110)
 			begin
+			if (negative2 == 1'b1) operand2 =-operand2;
+			if (negative1 == 1'b1) operand1 =-operand1;
 			case (operator)
 				2'b00 : data_out <= operand1 + operand2;
 				2'b01 : data_out <= operand1 - operand2;	
@@ -126,14 +101,19 @@ always @(negedge(sel))
 			endcase
 		end
 	end
+	endcase
 	
-	if (scan_counter < 9) scan_counter = scan_counter + 1;
-	else 
-		begin
-		scan_counter = 0;
-		n_enter = 0;
-		end
-
+	if (counter == 9) begin
+	counter <= 0;
+	temp <= 0;
+	temp1 <=0;
+	operand1 <=0;
+	operand2 <=0;
+	end
+	
+	if (data_in!=11'b1110) counter <= counter+1;
+		
 	end // always @(negedge(sel))
-
+	
+	
 endmodule
