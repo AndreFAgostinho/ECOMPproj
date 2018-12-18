94babe3caeb15679b38ea3a03b0fc0e7ec3cf4a1
micasserodio <micaelaserodio08@gmail.com>
2018-12-18 16:42:47 +0000

    não é relevante!

--- Code/rtl/xtop.v
+++ Code/rtl/xtop.v
@@ -17,15 +17,12 @@ module xtop (
 		  input				ps2_clk,
 		  
 
-		  // Push button signals
-		  input				push_AC,
-		  input				push_C,
 		  
 		  // 7 segment display control signals
 		  output	[11:0]	disp_ctrl,
           
           // LED ouput
-          output [7:0]	      gpo_out
+          output [7:0]	      overflow_led
 		  
 	     );
 
@@ -60,14 +57,9 @@ module xtop (
 	reg					ps2_sel;
     reg                 ps2_rst;
 	wire [8:0]			ps2_data_to_rd;
-	reg					pushs_sel;
-    reg                 pushs_rst;
-	wire [1:0]			pushs_data_to_rd;
   	reg					disp_sel;
     reg                 disp_rst;
-    reg                 gpo_sel;
-    reg                 gpo_rst;
-	reg 				oper_sel;
+	reg 					oper_sel;
 	wire [10:0]			oper_data_to_rd;
 
 
@@ -124,17 +116,14 @@ module xtop (
 
    // ADDRESS DECODER
    always @ * begin
-        prog_sel = 1'b0;
-        regf_sel = 1'b0;
-		pushs_sel = 1'b0;
-        pushs_rst = 1'b0;
+      prog_sel = 1'b0;
+      regf_sel = 1'b0;
 		ps2_sel = 1'b0;
-        ps2_rst = 1'b0;
+      ps2_rst = 1'b0;
 		disp_sel =1'b0;
-        disp_rst = 1'b0;
-        gpo_sel = 1'b0;
-        gpo_rst = 1'b0;
+      disp_rst = 1'b0;
 		oper_sel =1'b0;
+	
 		
 `ifdef DEBUG
         cprt_sel = 1'b0;
@@ -142,7 +131,7 @@ module xtop (
         data_to_rd = `DATA_W'd0;
       
         if (`REGF_BASE == (data_addr & ({`ADDR_W{1'b1}}<<`REGF_ADDR_W))) begin
-	        regf_sel = data_sel;
+				regf_sel = data_sel;
             data_to_rd = regf_data_to_rd;
         end
 
@@ -156,10 +145,6 @@ module xtop (
             data_to_rd = prog_data_to_rd;	    
         end
 
-	    else if (`PUSH_BASE == data_addr) begin
-	        pushs_sel = 1'b1;
-			  data_to_rd = pushs_data_to_rd;
-	    end
 	    else if(`PS2_BASE == data_addr) begin
 	        ps2_sel =1'b1;
 			  data_to_rd = ps2_data_to_rd;
@@ -168,8 +153,7 @@ module xtop (
 	  else if(`DISP_BASE == data_addr)
 	      disp_sel =1'b1;
 			  
-        else if (`GPO_BASE == data_addr)
-	        gpo_sel = 1'b1;
+       
 	else if (`OPER_BASE == data_addr) 
 		oper_sel =1'b1;
 	else if (`OP_BASE == data_addr) begin
@@ -225,16 +209,6 @@ module xtop (
 			.data_out(ps2_data_to_rd)
 			);
 			
-	xpushs pushs (
-			.clk(clk),
-			.sel(pushs_sel),
-			.rst(rst),
-			.push_C(push_C),
-			.push_AC(push_AC),
-			.data_out(pushs_data_to_rd)
-			);
-
-
 			
 	xdisp disp (
 			.clk(clk),
@@ -244,20 +218,15 @@ module xtop (
 			.data_out(disp_ctrl)
 			);
 
-    xgpo gpo (
-	    .clk(clk),
-        .sel(gpo_sel),
-	    .rst(gpo_rst),
-	    .data_in(data_to_wr[7:0]),
-	    .data_out(gpo_out)
-	    );
+ 
 
 xoper oper (
 	    .clk(clk),
-            .sel(oper_sel),
+       .sel(oper_sel),
 	    .rst(rst),
 	    .data_in(data_to_wr[10:0]),
-	    .data_out(oper_data_to_rd)
+	    .data_out(oper_data_to_rd),
+		 .led(overflow_led)
 	    );
 	
 			
