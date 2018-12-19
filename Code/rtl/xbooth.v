772d2faef37179e99a78f2d1687a1f81682ee59a
André Agostinho <andre.f.agostinho@tecnico.ulisboa.pt>
2018-12-19 10:25:42 +0000

    Linting and documenting xbooth.v

--- Code/rtl/xbooth.v
+++ Code/rtl/xbooth.v
@@ -1,6 +1,12 @@
+/////////////////////////////////////////////////////////////////////////////
+// 12 x 12 bit booth radix-4 multiplier for signed integers
+// Pulse start to start multiplying (hold start high for less than 6 cycles)
+// When the multiplication is done (always after 6 cycles) 
+// the ready output goes high
+/////////////////////////////////////////////////////////////////////////////
 module booth_mult(prod,ready,multiplicand,multiplier,start,clk);
 
-   input [11:0]  multiplicand, multiplier;
+	input [11:0]  multiplicand, multiplier;
    input         start, clk;
    output        prod;
    output        ready;
@@ -9,35 +15,38 @@ module booth_mult(prod,ready,multiplicand,multiplier,start,clk);
    wire [23:0]   prod = product[23:0];
 
 
-   reg [4:0]     bit = 0; 
-   wire          ready = !bit;
+   reg [4:0]     bit_index = 0; 
+   wire          ready = !bit_index;
    reg           lostbit = 0;
    
 
    wire [12:0]   multsx = {multiplicand[11],multiplicand}; // signal extended multiplicand
 
-   always @( posedge clk )
+	always @( posedge clk )
 
-     if( ready && start ) begin
+		if( ready && start ) begin
 
-        bit     = 6;
-        product = { 13'd0, multiplier };
+        bit_index = 6; // iterate 6 groups of two bits
+        product = { 13'b0, multiplier };
         lostbit = 0;
         
-     end else if( bit ) begin
+		end 
+		else if( bit_index ) begin
 
-        case ( {product[1:0],lostbit} )
-          3'b001: product[24:12] = product[24:12] + multsx;
-          3'b010: product[24:12] = product[24:12] + multsx;
-          3'b011: product[24:12] = product[24:12] + 2 * multiplicand;
-          3'b100: product[24:12] = product[24:12] - 2 * multiplicand;
-          3'b101: product[24:12] = product[24:12] - multsx;
-          3'b110: product[24:12] = product[24:12] - multsx;
-        endcase
+			case ( {product[1:0],lostbit} )
+				// 3'b000: do nothing
+				3'b001: product[24:12] = product[24:12] + multsx; // +M
+				3'b010: product[24:12] = product[24:12] + multsx; // +M
+				3'b011: product[24:12] = product[24:12] + (multiplicand << 1); // +2M
+				3'b100: product[24:12] = product[24:12] - (multiplicand << 1); // -2M
+				3'b101: product[24:12] = product[24:12] - multsx; // -M
+				3'b110: product[24:12] = product[24:12] - multsx; // -M
+				// 3'b111: do nothing
+			endcase
 
-        lostbit = product[1];
-        product = { product[24], product[24], product[24:2] };
-        bit     = bit - 1;
+			  lostbit = product[1];
+			  product = { product[24], product[24], product[24:2] };
+			  bit_index = bit_index - 1;
 
      end
 
